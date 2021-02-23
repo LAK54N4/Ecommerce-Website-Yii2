f64a08b9db86aee370e86bc31d4695ee97ffce3d
LAK54N4 <maslaksana@gmail.com>
2021-02-23 23:50:16 +0700

    Generate ActiveRecord models and queries

---
+++ common/models/Order.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%orders}}".
+ *
+ * @property int $id
+ * @property float $total_price
+ * @property int $status
+ * @property string $firstName
+ * @property string $lastName
+ * @property string $email
+ * @property int $transaction_id
+ * @property int|null $created_at
+ * @property int|null $created_by
+ *
+ * @property OrderAddresses $orderAddresses
+ * @property OrderItems[] $orderItems
+ * @property User $createdAt
+ * @property User $createdBy
+ */
+class Order extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%orders}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['total_price', 'status', 'firstName', 'lastName', 'email', 'transaction_id'], 'required'],
+            [['total_price'], 'number'],
+            [['status', 'transaction_id', 'created_at', 'created_by'], 'integer'],
+            [['firstName', 'lastName'], 'string', 'max' => 45],
+            [['email'], 'string', 'max' => 255],
+            [['created_at'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['created_at' => 'id']],
+            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['created_by' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'total_price' => 'Total Price',
+            'status' => 'Status',
+            'firstName' => 'First Name',
+            'lastName' => 'Last Name',
+            'email' => 'Email',
+            'transaction_id' => 'Transaction ID',
+            'created_at' => 'Created At',
+            'created_by' => 'Created By',
+        ];
+    }
+
+    /**
+     * Gets query for [[OrderAddresses]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\OrderAddressesQuery
+     */
+    public function getOrderAddresses()
+    {
+        return $this->hasOne(OrderAddresses::className(), ['order_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[OrderItems]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\OrderItemsQuery
+     */
+    public function getOrderItems()
+    {
+        return $this->hasMany(OrderItems::className(), ['order_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[CreatedAt]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\UserQuery
+     */
+    public function getCreatedAt()
+    {
+        return $this->hasOne(User::className(), ['id' => 'created_at']);
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\UserQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'created_by']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return \common\models\query\OrderQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \common\models\query\OrderQuery(get_called_class());
+    }
+}
