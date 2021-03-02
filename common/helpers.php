7c667d0f9b407f4d89fc3245913dc1a1667afa08
LAK54N4 <maslaksana@gmail.com>
2021-03-02 22:49:09 +0700

    Implement add in cart for guest Implement delete item from cart

---
+++ common/helpers.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 
+ */
+ 
+
+ 
+function isGuest()
+{
+    return Yii::$app->user->isGuest;
+}
+
+function currUserId()
+{
+    return Yii::$app->user->id;
+}
+
+function param($key)
+{
+    return Yii::$app->params[$key];
+}
